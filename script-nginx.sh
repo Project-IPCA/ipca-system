2a637349da4b6321b09275229f186760405386bd
Pattaradanai Kamsuan <pattaradanai4290@gmail.com>
2025-03-30 23:26:53 +0700

    Update environment variable substitution in Nginx script to simplify configuration

--- script-nginx.sh
+++ script-nginx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Substitute environment variables in the Nginx configuration template
-envsubst '\${SUPER_SERVER_NAME} \${STUDENT_SERVER_NAME} \${SSL_CERTIFICATE} \${SSL_CERTIFICATE_KEY}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '\${SERVER_NAME} \${SSL_CERTIFICATE} \${SSL_CERTIFICATE_KEY}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 # Start Nginx
 nginx -g 'daemon off;'
